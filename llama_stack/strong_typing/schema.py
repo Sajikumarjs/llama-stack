ef28abe0271f5bad37bd625fc80cdc0e0a9e9291
Sajikumar JS <sajikumar.js@ibm.com>
2025-04-26 00:54:21 +0530

    check

--- llama_stack/strong_typing/schema.py
+++ llama_stack/strong_typing/schema.py
@@ -417,8 +417,6 @@ class JsonSchemaGenerator:
             (list_type,) = typing.get_args(typ)  # unpack single tuple element
             return {"type": "array", "items": self.type_to_schema(list_type)}
         elif origin_type is dict:
-            print("=====================")
-            print(typ)
             key_type, value_type = typing.get_args(typ)
             if not (key_type is str or key_type is int or is_type_enum(key_type)):
                 raise ValueError("`dict` with key type not coercible to `str` is not supported")
